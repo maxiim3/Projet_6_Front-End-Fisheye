2621a00ef6304c7104beab5533f903f412a5e8ca
Maxime Tamburrini <maximemickael.contact@gmail.com>
2022-08-09 18:26:20 +0200

    reformat styles

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,3 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+// Todo recuperer les elements du contact modal et appeler les fonctions presentes dans le HTML cf Todo photographer.html
